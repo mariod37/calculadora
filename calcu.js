3bb2130e97eca22bd4292df8a42dc56d475aacbb
mario da silva <mariodasilva201215@gmail.com>
2024-05-20 23:07:52 -0300

    arreglo una y desarreglo otra ja ja

--- calcu.js
+++ calcu.js
@@ -1,50 +1,57 @@
-const displayDelete = document.querySelector('.displayDelete') 
-const display   = document.querySelector('input')
-const operador  = document.querySelectorAll('#operador')
-const resultado = document.querySelector('.btn-resultado')
-const numero    = document.querySelectorAll('.btn-numero')
-const conteiner = document.querySelector('.conteiner')
-const borrar = document.querySelector('.btn-delete')
-const porcentaje = document.querySelector('#porcentaje');
-console.log(porcentaje.value)
-let num1 = display.value
-let num2 = display.value
-
+const displayDelete = document.querySelector(".displayDelete");
+let display1 = document.querySelector("input");
+// const display2   = document.querySelector('display2')
+const operador = document.querySelectorAll("#operador");
+let resultado = document.querySelector(".btn-resultado");
+const numero = document.querySelectorAll(".btn-numero");
+const conteiner = document.querySelector(".conteiner");
+const borrar = document.querySelector(".btn-delete");
+let porcentaje = document.querySelector("#porcentaje");
+// console.log(porcentaje.value)
+// let resultado2 = document.querySelector('.btn-resultado')
+let valor1 = 0;
+let valor2 = 0;
 
 // console.log(operador)
 
-for (let i = 0; i < numero.length; i++){
-    numero[i].addEventListener('click', ()=>{
+for (let i = 0; i < numero.length; i++) {
+  numero[i].addEventListener("click", () => {
     // console.log(numero[i].value)
-    display.value+=numero[i].value
-    })
+    display1.value += numero[i].value;
+  });
 }
 
 
-    conteiner.addEventListener('click',(e) =>{
-            if (e.target && e.target.id === 'operador'){
-            display.value += e.target.value
-    }
-            console.log(e.target.value)
+
+conteiner.addEventListener("click", (e) => {
+  if (
+    e.target &&
+    e.target.id === "operador"
+    // e.target && e.target.id ==='porcentaje'
+  ) {
+    display1.value += e.target.value;
+  }
+  // console.log(e.target.value)
+});
+
+displayDelete.addEventListener("click", () => {
+  display1.value = "";
+});
+
+borrar.addEventListener("click", () => {
+  return (display1.value = display1.value.slice(0, -1));
+});
+
+porcentaje.addEventListener("click", () => {
+  valor1 = display1.value;
+  display1.value = "";
+  console.log(valor1);
+});
+
+resultado.addEventListener("click", (opera) => {
+ valor2 = display1.value
+  opera = eval((valor1 * valor2) / 100);
+  console.log(eval(valor2));
+  display1.value = opera;
+  
 })
-
-    resultado.addEventListener('click', function(){
-    
-            display.value = eval(display.value)
-})
-
-    displayDelete.addEventListener('click',() =>{
-    display.value = ''
-})
-
-    borrar.addEventListener('click', () =>{
-            
-                return display.value = display.value.slice(0,-1)
-})
-
-
-
-
-
-
-

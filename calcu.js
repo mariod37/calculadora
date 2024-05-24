74342ff31f7f26ac7f61f6d3ae1ebbfe147484db
mariod37 <mariodasilva201215@gmail.com>
2024-05-24 00:01:20 -0300

    Update calcu.js
    Projs

--- calcu.js
+++ calcu.js
@@ -1,33 +1,63 @@
-
+const displayDelete = document.querySelector(".displayDelete");
 let display1 = document.querySelector("input");
-const buttons = document.querySelectorAll("button");
-let esp = ['+','-','*','/','%']
-let salida = '';
+// const display2   = document.querySelector('display2')
+const operador = document.querySelectorAll("#operador");
+let resultado = document.querySelector(".btn-resultado");
+const numero = document.querySelectorAll(".btn-numero");
+const conteiner = document.querySelector(".conteiner");
+const borrar = document.querySelector(".btn-delete");
+let porcentaje = document.querySelector("#porcentaje");
+// console.log(porcentaje.value)
+// let resultado2 = document.querySelector('.btn-resultado')
+let valor1 = 0;
+let valor2 = 0;
+
+// console.log(operador)
+
+for (let i = 0; i < numero.length; i++) {
+  numero[i].addEventListener("click", () => {
+    // console.log(numero[i].value)
+    display1.value += numero[i].value;
+  });
+}
 
 
-const calculate = (btnValue) =>{
 
-  if(btnValue === '='&& salida !==''){
-    
-    salida = eval(salida.replace("%","/100"))
-    console.log('siii')
+conteiner.addEventListener("click", (e) => {
+  if (
+    e.target &&
+    e.target.id === "operador"
+    // e.target && e.target.id ==='porcentaje'
+  ) {
+    display1.value += e.target.value;
   }
-else if(btnValue === 'AC'){
-  salida = ''
-}
-else if(btnValue === 'Del'){
-  salida = salida.toString().slice(0, -1)
-}
-else{
-  if(salida === ''&& esp.includes(btnValue))return;
-  salida+= btnValue;
-}
-  display1.value = salida
-    
-    
-}
-buttons.forEach((button) =>
-{
-  button.addEventListener('click', (e) => calculate(e.target.dataset.value))
-}) 
+  // console.log(e.target.value)
+});
 
+displayDelete.addEventListener("click", () => {
+  display1.value = "";
+});
+
+borrar.addEventListener("click", () => {
+  return (display1.value = display1.value.slice(0, -1));
+});
+let esPorcentaje = false;
+porcentaje.addEventListener("click", () => {
+  valor1 = display1.value;
+  display1.value = "";
+  console.log(valor1);
+  esPorcentaje = true
+});
+
+resultado.addEventListener("click", (opera) => {
+ valor2 = display1.value
+  if(esPorcentaje)
+  opera = eval((valor1 * valor2) / 100);
+  
+  else
+    opera = eval(valor2)
+  console.log(eval(valor2));
+  display1.value = opera;
+    esPorcentaje = false;
+  
+})

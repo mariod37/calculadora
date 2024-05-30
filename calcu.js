006d79b2233a6e4b74e12a5d2418cf930eb8f26d
mariod37 <mariodasilva201215@gmail.com>
2024-05-29 23:30:17 -0300

    Update calcu.js
    commit manual

--- calcu.js
+++ calcu.js
@@ -1,18 +1,21 @@
 const displayDelete = document.querySelector(".displayDelete");
 let display1 = document.querySelector("input");
-// const display2   = document.querySelector('display2')
 const operador = document.querySelectorAll("#operador");
-let resultado = document.querySelector(".btn-resultado");
-const numero = document.querySelectorAll(".btn-numero");
+let resultado = document.querySelector(".boton-resultado");
+const numero = document.querySelectorAll(".boton-numero");
 const conteiner = document.querySelector(".conteiner");
-const borrar = document.querySelector(".btn-delete");
+const borrar = document.querySelector(".boton-delete");
 let porcentaje = document.querySelector("#porcentaje");
-// console.log(porcentaje.value)
-// let resultado2 = document.querySelector('.btn-resultado')
+let historial = document.querySelector(".historial");
+let valorHistorial = [];
+let localSto = localStorage;
+var datos;
+let boton = document.querySelector('button');
 let valor1 = 0;
 let valor2 = 0;
 
-// console.log(operador)
+// valorHistorial = valorHistorial.push(display1.value)
+//  console.log(valorHistorial)
 
 for (let i = 0; i < numero.length; i++) {
   numero[i].addEventListener("click", () => {
@@ -20,19 +23,18 @@ for (let i = 0; i < numero.length; i++) {
     display1.value += numero[i].value;
   });
 }
+//  conteiner.addEventListener("click", (e) => {
+//   if (
+//     e.target &&
+//     e.target.id === "operador"
+//     // e.target && e.target.id ==='porcentaje'
+//   ) {
+//     display1.value += e.target.value;
+//   }
+//   console.log()
+  
+// });
 
-
-
-conteiner.addEventListener("click", (e) => {
-  if (
-    e.target &&
-    e.target.id === "operador"
-    // e.target && e.target.id ==='porcentaje'
-  ) {
-    display1.value += e.target.value;
-  }
-  // console.log(e.target.value)
-});
 
 displayDelete.addEventListener("click", () => {
   display1.value = "";
@@ -46,18 +48,46 @@ porcentaje.addEventListener("click", () => {
   valor1 = display1.value;
   display1.value = "";
   console.log(valor1);
-  esPorcentaje = true
+  
+  esPorcentaje = true;
 });
 
+
 resultado.addEventListener("click", (opera) => {
- valor2 = display1.value
-  if(esPorcentaje)
-  opera = eval((valor1 * valor2) / 100);
+  valor2 = display1.value;
+  if (esPorcentaje) opera = eval((valor1 * valor2) / 100);
+  else opera = eval(valor2);
   
-  else
-    opera = eval(valor2)
-  console.log(eval(valor2));
   display1.value = opera;
-    esPorcentaje = false;
+  // localSto.setItem("operacion", datos );
   
-})
+  // valorHistorial.push(localSto.operacion);
+  // console.log(valorHistorial);
+  
+  esPorcentaje = false;
+});
+
+
+conteiner.addEventListener("click", (e) => {
+  
+  
+    if (
+      e.target &&
+      e.target.id === "operador"
+      // e.target && e.target.id ==='porcentaje'
+    ) {
+      display1.value += e.target.value;
+    }
+  datos = display1.value 
+  localSto.setItem("operacion", datos );
+  
+  valorHistorial.push(localSto.operacion);
+  console.log(valorHistorial);
+    // console.log(e.target.value)
+    console.log(datos)
+  });
+  
+  boton.addEventListener('click', () =>{
+    historial.innerHTML = `${valorHistorial}`
+  })
+
